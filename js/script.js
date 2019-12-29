15abe3fff95496c90a2dfe7225c8f545cc455344
Fatem Ataee <fatemehataee95@gmail.com>
2019-12-29 12:50:31 +0330

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        item:1,
+        merge:true,
+        loop:true,
+        center:true, 
+        nav:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    });
+  });
